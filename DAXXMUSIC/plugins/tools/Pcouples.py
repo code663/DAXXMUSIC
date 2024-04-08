a598f31df6517fd4a2a5858663ad134a58b76620
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-04-08 14:27:22 +0530

    Update Pcouples.py

--- DAXXMUSIC/plugins/tools/Pcouples.py
+++ DAXXMUSIC/plugins/tools/Pcouples.py
@@ -100,8 +100,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (495, 574), img1)
-         img.paste(img2, (2495, 574), img2)
+         img.paste(img1, (500, 585), img1)
+         img.paste(img2, (2500, 585), img2)
 
          img.save(f'test_{cid}.png')
     
